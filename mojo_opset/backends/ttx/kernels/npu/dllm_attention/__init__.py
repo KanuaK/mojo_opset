fix: fix kv_ur precision problem

mojo_opset/backends/ttx/kernels/npu/dllm_attention/__init__.py
@@ -13,7 +13,7 @@ from .bwd_d import kernel_da_bwd_d
 from .bwd_q_u import kernel_da_bwd_q_u, kernel_da_bwd_q_u_single
 from .bwd_q_d import kernel_da_bwd_q_d
 from .bwd_kv_ul import kernel_da_bwd_kv_ul
-from .bwd_kv_ur import kernel_da_bwd_kv_ur
+from .bwd_kv_ur import kernel_da_bwd_kv_ur, kernel_da_bwd_kv_ur_single
 from .bwd_kv_r import kernel_da_bwd_kv_r
 
 
@@ -151,12 +151,9 @@ def dllm_attention_up_bwd_impl(
 
     num_cores, num_vectorcore = get_device_properties()
     d = torch.empty(q.shape[1], q.shape[0], device=q.device, dtype=torch.float32).T
-    # dq = torch.zeros_like(q)
-    # dk = torch.zeros_like(k)
-    # dv = torch.zeros_like(v)
-    dq = torch.empty_like(q)
-    dk = torch.empty_like(k)
-    dv = torch.empty_like(v)
+    dq = torch.zeros_like(q)
+    dk = torch.zeros_like(k)
+    dv = torch.zeros_like(v)
 
     if (not hasattr(dllm_attention_up_bwd_impl, "masks")):
         dllm_attention_up_bwd_impl.masks = {}
@@ -184,6 +181,7 @@ def dllm_attention_up_bwd_impl(
         d.stride(0),
         d.stride(1),
     )
+    # kernel_da_bwd_q_u_single[(num_cores,)](
     kernel_da_bwd_q_u(
         q,
         k,
@@ -245,7 +243,7 @@ def dllm_attention_up_bwd_impl(
         d.stride(1),
         mask_ul.stride(0),
     )
-    # kernel_da_bwd_kv_ur[(num_cores,)](
+    # kernel_da_bwd_kv_ur_single[(num_cores,)](
     kernel_da_bwd_kv_ur(
         q,
         k,

mojo_opset/backends/ttx/kernels/npu/dllm_attention/bwd_q_u.py
@@ -12,11 +12,11 @@ from .micro_kernel import micro_kernel_bwd_q
         #     tile_mix_vector_loop=2,
         #     tile_mix_cube_loop=2,
         # ),
-        triton.Config(
-            {"BLOCK_R": 64},
-            tile_mix_vector_loop=2,
-            tile_mix_cube_loop=4,
-        ),
+        # triton.Config(
+        #     {"BLOCK_R": 64},
+        #     tile_mix_vector_loop=2,
+        #     tile_mix_cube_loop=4,
+        # ),
         # triton.Config(
         #     {"BLOCK_R": 64},
         #     tile_mix_vector_loop=2,
@@ -51,6 +51,12 @@ from .micro_kernel import micro_kernel_bwd_q
         #     {"BLOCK_R": 64},
         #     tile_mix_vector_loop=8,
         #     tile_mix_cube_loop=8,
+        # ),
+        triton.Config(
+            {"BLOCK_R": 64},
+        ),
+        # triton.Config(
+        #     {"BLOCK_R": 128},
         # ),
     ],
     key=["N", "H"],
@@ -179,11 +185,11 @@ def _kernel_bwd_q_u_masked_ul(
         #     tile_mix_vector_loop=2,
         #     tile_mix_cube_loop=2,
         # ),
-        triton.Config(
-            {"BLOCK_R": 64},
-            tile_mix_vector_loop=2,
-            tile_mix_cube_loop=4,
-        ),
+        # triton.Config(
+        #     {"BLOCK_R": 64},
+        #     tile_mix_vector_loop=2,
+        #     tile_mix_cube_loop=4,
+        # ),
         # triton.Config(
         #     {"BLOCK_R": 64},
         #     tile_mix_vector_loop=2,
@@ -218,6 +224,12 @@ def _kernel_bwd_q_u_masked_ul(
         #     {"BLOCK_R": 64},
         #     tile_mix_vector_loop=8,
         #     tile_mix_cube_loop=8,
+        # ),
+        triton.Config(
+            {"BLOCK_R": 64},
+        ),
+        # triton.Config(
+        #     {"BLOCK_R": 128},
         # ),
     ],
     key=["N", "H"],
@@ -361,11 +373,11 @@ def _kernel_bwd_q_u_masked_ur(
         #     tile_mix_vector_loop=4,
         #     tile_mix_cube_loop=2,
         # ),
-        triton.Config(
-            {"BLOCK_R": 64, "BLOCK_C": 256},
-            tile_mix_vector_loop=4,
-            tile_mix_cube_loop=4,
-        ),
+        # triton.Config(
+        #     {"BLOCK_R": 64, "BLOCK_C": 256},
+        #     tile_mix_vector_loop=4,
+        #     tile_mix_cube_loop=4,
+        # ),
         # triton.Config(
         #     {"BLOCK_R": 64, "BLOCK_C": 256},
         #     tile_mix_vector_loop=4,
@@ -385,6 +397,12 @@ def _kernel_bwd_q_u_masked_ur(
         #     {"BLOCK_R": 64, "BLOCK_C": 256},
         #     tile_mix_vector_loop=8,
         #     tile_mix_cube_loop=8,
+        # ),
+        triton.Config(
+            {"BLOCK_R": 64, "BLOCK_C": 256},
+        ),
+        # triton.Config(
+        #     {"BLOCK_R": 128, "BLOCK_C": 512},
         # ),
     ],
     key=["N", "H"],
@@ -498,53 +516,56 @@ def _kernel_bwd_q_u_residual(
 
 @triton.autotune(
     configs=[
-        triton.Config(
-            {"BLOCK_R": 64, "BLOCK_C": 256},
-            tile_mix_vector_loop=2,
-            tile_mix_cube_loop=2,
-        ),
-        triton.Config(
-            {"BLOCK_R": 64, "BLOCK_C": 256},
-            tile_mix_vector_loop=2,
-            tile_mix_cube_loop=4,
-        ),
-        triton.Config(
-            {"BLOCK_R": 64, "BLOCK_C": 256},
-            tile_mix_vector_loop=2,
-            tile_mix_cube_loop=8,
-        ),
-        triton.Config(
-            {"BLOCK_R": 64, "BLOCK_C": 256},
-            tile_mix_vector_loop=4,
-            tile_mix_cube_loop=2,
-        ),
-        triton.Config(
-            {"BLOCK_R": 64, "BLOCK_C": 256},
-            tile_mix_vector_loop=4,
-            tile_mix_cube_loop=4,
-        ),
-        triton.Config(
-            {"BLOCK_R": 64, "BLOCK_C": 256},
-            tile_mix_vector_loop=4,
-            tile_mix_cube_loop=8,
-        ),
-        triton.Config(
-            {"BLOCK_R": 64, "BLOCK_C": 256},
-            tile_mix_vector_loop=8,
-            tile_mix_cube_loop=2,
-        ),
-        triton.Config(
-            {"BLOCK_R": 64, "BLOCK_C": 256},
-            tile_mix_vector_loop=8,
-            tile_mix_cube_loop=4,
-        ),
-        triton.Config(
-            {"BLOCK_R": 64, "BLOCK_C": 256},
-            tile_mix_vector_loop=8,
-            tile_mix_cube_loop=8,
-        ),
         # triton.Config(
         #     {"BLOCK_R": 64, "BLOCK_C": 256},
+        #     tile_mix_vector_loop=2,
+        #     tile_mix_cube_loop=2,
+        # ),
+        # triton.Config(
+        #     {"BLOCK_R": 64, "BLOCK_C": 256},
+        #     tile_mix_vector_loop=2,
+        #     tile_mix_cube_loop=4,
+        # ),
+        # triton.Config(
+        #     {"BLOCK_R": 64, "BLOCK_C": 256},
+        #     tile_mix_vector_loop=2,
+        #     tile_mix_cube_loop=8,
+        # ),
+        # triton.Config(
+        #     {"BLOCK_R": 64, "BLOCK_C": 256},
+        #     tile_mix_vector_loop=4,
+        #     tile_mix_cube_loop=2,
+        # ),
+        # triton.Config(
+        #     {"BLOCK_R": 64, "BLOCK_C": 256},
+        #     tile_mix_vector_loop=4,
+        #     tile_mix_cube_loop=4,
+        # ),
+        # triton.Config(
+        #     {"BLOCK_R": 64, "BLOCK_C": 256},
+        #     tile_mix_vector_loop=4,
+        #     tile_mix_cube_loop=8,
+        # ),
+        # triton.Config(
+        #     {"BLOCK_R": 64, "BLOCK_C": 256},
+        #     tile_mix_vector_loop=8,
+        #     tile_mix_cube_loop=2,
+        # ),
+        # triton.Config(
+        #     {"BLOCK_R": 64, "BLOCK_C": 256},
+        #     tile_mix_vector_loop=8,
+        #     tile_mix_cube_loop=4,
+        # ),
+        # triton.Config(
+        #     {"BLOCK_R": 64, "BLOCK_C": 256},
+        #     tile_mix_vector_loop=8,
+        #     tile_mix_cube_loop=8,
+        # ),
+        triton.Config(
+            {"BLOCK_R": 64, "BLOCK_C": 256},
+        ),
+        # triton.Config(
+        #     {"BLOCK_R": 128, "BLOCK_C": 256},
         # ),
     ],
     key=["N", "H"],
@@ -903,11 +924,10 @@ def kernel_da_bwd_q_u(
         STRIDE_K_S, STRIDE_K_N, STRIDE_K_H,
         STRIDE_V_S, STRIDE_V_N, STRIDE_V_H,
         STRIDE_D_S, STRIDE_D_N, STRIDE_MASK,
-        enable_ubuf_saving=True,
-        multibuffer=True,
-        unit_flag=True,
         limit_auto_multi_buffer_only_for_local_buffer=False,
         set_workspace_multibuffer=4,
+        tile_mix_vector_loop=2,
+        tile_mix_cube_loop=4,
     )
     _kernel_bwd_q_u_masked_ur[(num_cores,)](
         q, k, v, do, d, lse, dq_masked_ur,
@@ -917,11 +937,10 @@ def kernel_da_bwd_q_u(
         STRIDE_K_S, STRIDE_K_N, STRIDE_K_H,
         STRIDE_V_S, STRIDE_V_N, STRIDE_V_H,
         STRIDE_D_S, STRIDE_D_N, STRIDE_MASK,
-        enable_ubuf_saving=True,
-        multibuffer=True,
-        unit_flag=True,
         limit_auto_multi_buffer_only_for_local_buffer=False,
         set_workspace_multibuffer=4,
+        tile_mix_vector_loop=2,
+        tile_mix_cube_loop=4,
     )
     _kernel_bwd_q_u_residual[(num_cores,)](
         q, k, v, do, d, lse, dq_residual,
@@ -931,11 +950,10 @@ def kernel_da_bwd_q_u(
         STRIDE_K_S, STRIDE_K_N, STRIDE_K_H,
         STRIDE_V_S, STRIDE_V_N, STRIDE_V_H,
         STRIDE_D_S, STRIDE_D_N,
-        enable_ubuf_saving=True,
-        multibuffer=True,
-        unit_flag=True,
         limit_auto_multi_buffer_only_for_local_buffer=False,
         set_workspace_multibuffer=4,
+        tile_mix_vector_loop=2,
+        tile_mix_cube_loop=4,
     )
     _kernel_bwd_q_u_aligned[(num_cores,)](
         q, k, v, do, d, lse, dq_aligned,
@@ -945,11 +963,10 @@ def kernel_da_bwd_q_u(
         STRIDE_K_S, STRIDE_K_N, STRIDE_K_H,
         STRIDE_V_S, STRIDE_V_N, STRIDE_V_H,
         STRIDE_D_S, STRIDE_D_N,
-        enable_ubuf_saving=True,
-        multibuffer=True,
-        unit_flag=True,
-        limit_auto_multi_buffer_only_for_local_buffer=False,
-        set_workspace_multibuffer=4,
+        # limit_auto_multi_buffer_only_for_local_buffer=False,
+        # set_workspace_multibuffer=4,
+        # tile_mix_vector_loop=2,
+        # tile_mix_cube_loop=4,
     )
 
-    dq[:] = (dq_masked_ul + dq_masked_ur + dq_residual + dq_aligned)#.to(torch.bfloat16)
+    dq[:] = (dq_masked_ul + dq_masked_ur + dq_residual + dq_aligned).to(torch.bfloat16)